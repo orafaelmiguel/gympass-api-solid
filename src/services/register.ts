367e8ff20b4b0eb0c18c656cabe78c96be57edf6
Rafael Miguel <rafaelmiguel0202@gmail.com>
2024-03-28 17:28:53 -0300

    implement repositories and change dependecies

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,5 +1,6 @@
 import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 interface RegisterUseCaseRequest {
     name: string,
@@ -17,11 +18,11 @@ export async function registerUseCase({ name, email, password }: RegisterUseCase
 
     if (userWithSameEmail) throw new Error('Email already exists.')
 
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash
-        }
+    const prismaUsersRepository = new PrismaUsersRepository()
+
+    await prismaUsersRepository.create({
+        name,
+        email,
+        password_hash
     })
 }
