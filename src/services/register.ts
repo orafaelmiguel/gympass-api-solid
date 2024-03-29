0942dcdcb87ce352008e64cc23a535a01effe192
Rafael Miguel <rafaelmiguel0202@gmail.com>
2024-03-28 22:17:33 -0300

    test:  make sure the password is being encrypted

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,6 +1,7 @@
 import { hash } from 'bcryptjs'
 import { UsersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
     name: string,
@@ -8,21 +9,27 @@ interface RegisterUseCaseRequest {
     password: string
 }
 
+interface RegisterUseCaseResponse {
+    user: User
+}
+
 export class RegisterUseCase {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     constructor(private usersRepository: UsersRepository) {}
 
-    async execute({ name, email, password }: RegisterUseCaseRequest) {
+    async execute({ name, email, password }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
         const password_hash = await hash(password, 6)
 
         const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
         if (userWithSameEmail) throw new UserAlreadyExistsError()
 
-        await this.usersRepository.create({
+        const user = await this.usersRepository.create({
             name,
             email,
             password_hash
         })
+
+        return { user }
     }
 }
--- src/services/tests/register.spec.ts
+++ src/services/tests/register.spec.ts
@@ -1,5 +1,34 @@
-import { expect, test } from 'vitest'
+import { it, describe, expect } from 'vitest'
+import { RegisterUseCase } from '../register'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-    expect(2 + 2).toBe(4)
+describe('Register Use case', () => {
+    it('should hash user password upon registration', async () => {
+        const registerUseCase = new RegisterUseCase({
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            async findByEmail(email) {
+                return null
+            },
+
+            async create(data) {
+                return {
+                    id: 'user-1',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    created_at: new Date()
+                }
+            }
+        })
+
+        const { user } = await registerUseCase.execute({
+            name: 'Test',
+            email: 'test@email.com',
+            password: 'password'
+        })
+
+        const isPasswordCorrectlyHashed = await compare('password', user.password_hash)
+
+        expect(isPasswordCorrectlyHashed).toBe(true)
+    })
 }) 
