7ae537be320d1ba0014c4d36180f9c7f58ad355f
Rafael Miguel <rafaelmiguel0202@gmail.com>
2024-04-03 15:30:41 -0300

    method to find user by id

--- src/repositories/in-memory/in-memory-users-repositories.ts
+++ src/repositories/in-memory/in-memory-users-repositories.ts
@@ -4,6 +4,15 @@ import { User, Prisma } from '@prisma/client'
 export class inMemoryUsersRepository implements UsersRepository {
     public items: User[] = []
 
+    async findById(id: string) {
+        const user = this.items.find(item => item.id == id)
+        if(!user) {
+            return null
+        }
+
+        return user
+    }
+
     async findByEmail(email: string) {
         const user = this.items.find(item => item.email == email)
 
