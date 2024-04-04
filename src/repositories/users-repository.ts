f8c1042080f6e0a258a9929f7b1d77286c2d3e47
Rafael Miguel <rafaelmiguel0202@gmail.com>
2024-04-04 20:47:14 -0300

    att

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,6 +1,5 @@
 import { Prisma, User } from "@prisma/client";
 
-
 export interface UsersRepository {
     findById(id: string): Promise<User | null>
     findByEmail(email: string): Promise<User | null>
