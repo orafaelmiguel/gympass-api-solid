16607a9529faf40b7514792d28cb320768c0ed22
Rafael Miguel <rafaelmiguel0202@gmail.com>
2024-04-07 00:10:19 -0300

    gym creation use case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,5 +1,6 @@
+import { randomUUID } from "node:crypto"
 import { GymsRepository } from "../gyms-repository"
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 
 export class InMemoryGymsRepository implements GymsRepository {
     public items: Gym[] = []
@@ -9,6 +10,22 @@ export class InMemoryGymsRepository implements GymsRepository {
         if(!gym) {
             return null
         }
+
+        return gym
+    }
+
+    async create(data: Prisma.GymCreateInput) {
+        const gym = {
+            id: randomUUID(),
+            title: data.title,
+            description: data.description ?? null,
+            phone: data.phone ?? null,
+            latitude: new Prisma.Decimal(data.latitude.toString()),
+            longitude: new Prisma.Decimal(data.longitude.toString()),
+            created_at: new Date()
+        }
+
+        this.items.push(gym)
 
         return gym
     }
